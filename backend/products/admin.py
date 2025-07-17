cd8cf7e81ba7abc8417e409c170ad0966c674970
khoahuynh133 <khoahuynhminh2005@gmail.com>
2025-07-17 12:53:44 +0700

    update_product

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Product  # Xóa Category nếu không có
+from .models import Product, Category  # Xóa Category nếu không có
 
 admin.site.register(Product)
+admin.site.register(Category)
