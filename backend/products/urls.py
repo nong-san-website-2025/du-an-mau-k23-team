d2e74c73118db2753b95e7af285148e70a3281fa
BuiVanTien <nhatnguyen2004nguyen@gmail.com>
2025-08-16 18:43:01 +0700

    uop data

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,8 +1,15 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet, CategoryViewSet
+from .views import ProductViewSet, CategoryViewSet, SearchAPIView
 
+# Khai báo router cho ViewSet
 router = DefaultRouter()
 router.register("categories", CategoryViewSet)
-router.register("", ProductViewSet)
+router.register("products", ProductViewSet)
 
-urlpatterns = router.urls
+# URL patterns
+urlpatterns = [
+    path("search/", SearchAPIView.as_view(), name="search"),  # API tìm kiếm
+    path("", include(router.urls)),  # Các API của Product & Category
+    path("<int:pk>/", ProductViewSet.as_view({'get': 'retrieve'})),  # Cho phép truy cập chi tiết sản phẩm qua /api/products/<id>/
+]
