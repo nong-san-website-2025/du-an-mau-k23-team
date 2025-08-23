f94d1e2e8474358e6a5e8da99f982de934185096
khoahuynh133 <khoahuynhminh2005@gmail.com>
2025-08-24 00:56:32 +0700

    update_create_superuser

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -1,16 +1,18 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
+# ---- Auto create superuser ----
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser(
+        username="admin",
+        email="admin@example.com",
+        password="admin123"
+    )
+    print("✅ Superuser created automatically")
